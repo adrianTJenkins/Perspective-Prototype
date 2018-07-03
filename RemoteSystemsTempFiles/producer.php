f42dadff926d5eae6229c8dbd945e117b51d7c93
adrianTJenkins <adrian.jenkins99@gmail.com>
2018-07-03 16:07:31 -0400

    BAD COMMIT
    disregard this commit, getting reverted, outdated

--- RemoteSystemsTempFiles/producer.php
+++ RemoteSystemsTempFiles/producer.php
@@ -1,10 +1,3 @@
-<?php
-
-include 'dbh-inc.php';
-
-session_start();
- ?>
-
 <!DOCTYPE html>
 <html>
 
@@ -12,35 +5,26 @@ session_start();
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Anaheim|Dosis" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/view.css">
-    <script src = "assets/js/jquery.min.js"></script>
 </head>
 
 <body>
     <section id="main">
         <header>
-        	<script src="assets/js/search.js" type="text/javascript"></script>
+        	<script src="search.js" type="text/javascript"></script>
     			<script src="https://apis.google.com/js/client.js?onload=onClientLoad" type="text/javascript"></script>
-            <h1><a href ="home.php">Perspective</a></h1>
-
+            <h1><a href ="home.html">Perspective</a></h1>
         	<div id="search-container">
-    			<form onsubmit = "search()">
-      				<input id = "keyword" type="text" placeholder="Enter keywords..." name = "q">
+    			<form>
+      				<input id = "keyword" type="text" onkeyup = "search()" placeholder="Enter keywords..." name = "q">
       				<button type="submit"><i class="fa fa-search"></i></button>
     			</form>
 
   			</div>
 
-        <div id = "adminLogout">
-        <?php
-          if(isset($_SESSION['u_id'])) {
-            echo $_SESSION['u_name'].'<form action = "assets/includes/logout-inc.php" method = "POST">
-              <button type = "submit" name = "submit">Log out</button>
-            </form>';
-          }
-        ?>
-</header>
-</div>
+  			<p id = "adminLogin">Are you a producing your own broadcast as an <strong>administrater</strong>? <a href = "signIn.html">Sign in</a><br>Don't have an account? <a href = "signUp.html">Sign up</a></p>
 
+
+        </header>
     </section>
     <div id=playback>
         <section id="videos">
@@ -69,36 +53,26 @@ session_start();
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/js/player.js"></script>
 
-<!--
-  <video id = "bigPlayer" controls style="width:640px;height:360px;"></video>
--->
-  <script>
-
-var youtubeSearch = null;
-  function searchCompleted(response) {
-  youtubeSearch = response;
-  for (var k=0; k<youtubeSearch.items.length; k++)
-  {
-    $('#videos').append('<div class = "boxes"><div class="vid" id="player' + k+ '"> </div></div>');
-          var player = new YT.Player('player' + k, {
-              height: '60px',
-              width: '90px',
-              videoId: youtubeSearch.items[k].id.videoId,
-              events: {
-                  'onReady': onPlayerReady,
-              }
-          });
-          var j = k+1;
-
-         $('#player' + k).after('<button id = "cam' + k+'" class = "camera" onclick = "ajax_post()")">Camera ' + j+ '</button>');
-
-  }
-  bindCameraButtons();
-  checkPlayers();
-
-
-  }
-  </script>
-
+	<video id = "bigPlayer" controls style="width:640px;height:360px;"></video>
+    <script>
+	var youtubeSearch = null;
+    function searchCompleted(response) {
+		youtubeSearch = response;
+		for (var k=0; k<youtubeSearch.items.length; k++)
+		{
+			$('#videos').append('<div class="vid" id="player' + k+ '"> </div>');
+            var player = new YT.Player('player' + k, {
+                height: '60px',
+                width: '90px',
+                videoId: youtubeSearch.items[k].id.videoId,
+                events: {
+                    'onReady': onPlayerReady,
+                }
+            });
+            var j = k+1;
+           $('#videos').append('<button class = "camera" onclick = "feedId("#player '+k+'")">Camera ' + j+ '</button>');
+		}
+    }
+    </script>
 </body>
 </html>
