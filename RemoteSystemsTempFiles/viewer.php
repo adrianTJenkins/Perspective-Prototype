34d486bb39b2fd37da2a9f57a577ddbe0770e040
adrianTJenkins <adrian.jenkins99@gmail.com>
2018-07-09 15:07:46 -0400

    set interval (FIXED ERROR on viewer)

--- RemoteSystemsTempFiles/viewer.php
+++ RemoteSystemsTempFiles/viewer.php
@@ -22,65 +22,18 @@
 
   			</div>
 
-
         </header>
 
     </section>
 
+    <script src="assets/js/player.js" type="text/javascript">console.log(a);</script>
+    
+
     <video id = "vidPlayer" width="999" height="500" controls>
-  <source src="feedId()" type="video/mp4">
+
+  <source src = <?php echo $vid ?>> type="video/mp4">
 Your browser does not support the video tag.
 </video>
-    <!--
-    <div id=playback>
-        <section id="videos">
 
-            <script>
-            // 2. This code loads the IFrame Player API code asynchronously.
-            var tag = document.createElement('script');
-
-            tag.src = "https://www.youtube.com/iframe_api";
-            var firstScriptTag = document.getElementsByTagName('script')[0];
-            firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-            // 3. This function creates an <iframe> (and YouTube player)
-            //    after the API code downloads.
-            var player;
-
-            // 4. The API will call this function when the video player is ready.
-            function onPlayerReady(event) {
-                event.target.playVideo();
-                event.target.mute();
-            }
-            </script>
-        </section>
-    </div>
-    <!-- Scripts
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/js/player.js"></script>
-
-    <script>
-
-	var youtubeSearch = null;
-    function searchCompleted(response) {
-		youtubeSearch = response;
-		for (var k=0; k<youtubeSearch.items.length; k++)
-		{
-			$('#videos').append('<div class = "boxes"><div class="vid" id="player' + k+ '"> </div></div>');
-            var player = new YT.Player('player' + k, {
-                height: '60px',
-                width: '90px',
-                videoId: youtubeSearch.items[k].id.videoId,
-                events: {
-                    'onReady': onPlayerReady,
-                }
-            });
-            var j = k+1;
-           $('#player' + k).after('<button id = "cam' + k+'" class = "camera")">Camera ' + j+ '</button>');
-		}
-    bindCameraButtons();
-    }
-    </script>
--->
 </body>
 </html>
